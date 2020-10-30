27123a0a7acc1eb5dd50d6be3a3fa63389de68f5
TsarChicken <tsarchicken@mail.ru>
2020-10-30 18:46:59 +0200

    Friday

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Main
 {
-
     private boolean running = false;
     private Board enemyBoard = new Board(true);
     private Board playerBoard = new Board(false);
@@ -13,9 +12,8 @@ public class Main
     private boolean enemyTurn = false;
 
     private Random random = new Random();
-    AI test = new AI();
-    AI test2 = new AI();
-
+    AI enemy = new AI();
+    int y, x;
     private void fillBoard(Board order, boolean isBegun){
         int type = 4;
 
@@ -41,28 +39,14 @@ public class Main
     }
 
     private void enemyMove(){
-        Cell cel12 = test.startThink(playerBoard);
-        System.out.println(playerBoard.grid[cel12.y][cel12.x].shoot());
-        System.out.println(cel12.x + " " + cel12.y);
-
-        showGrid(enemyBoard);
-        showGrid(playerBoard);
-
-        if (cel12.ship == null) {
-            enemyTurn = true;
-        }
+        Cell cel12 = enemy.startThink(playerBoard);
+        enemyTurn = cel12.shoot();
     }
 
-    private void playerMove(){
-        Cell cel12 = test2.startThink(enemyBoard);
-        System.out.println(enemyBoard.grid[cel12.y][cel12.x].shoot());
-        System.out.println(cel12.x + " " + cel12.y);
-
-        showGrid(enemyBoard);
-        showGrid(playerBoard);
-        if (cel12.ship == null) {
-            enemyTurn = false;
-        }
+    private void playerMove(int y, int x){
+        Cell cel12 = enemyBoard.getCell(y, x) ;
+        enemyTurn = !cel12.shoot();
+        cel12.isShipVisible = cel12.shoot();
     }
 
     private void gameProcess(){
@@ -71,7 +55,7 @@ public class Main
                 if (enemyTurn == false) {
                     enemyMove();
                 } else {
-                    playerMove();
+                    playerMove(y, x);
                 }
             }
             if (playerBoard.playerWon)
@@ -81,6 +65,7 @@ public class Main
             }
         }
     }
+
     private void createContent() {
         startGame();
         gameProcess();
@@ -93,41 +78,5 @@ public class Main
     {
         Main m1 = new Main();
         m1.createContent();
-    }
-
-    public void showGrid(Board a)
-    {
-        System.out.println("0123456789");
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-
-
-                if(a.grid[i][j].wasShot && a.grid[i][j].ship==null)
-                {
-                    System.out.print('1');
-                }
-                else if (a.grid[i][j].ship==null)
-                {
-                    System.out.print('.');
-                }
-
-                else if(a.grid[i][j].wasShot && a.grid[i][j].ship!=null)
-                {
-                    System.out.print('2');
-                }
-
-                else
-                {
-                    System.out.print('*');
-                }
-
-
-            }
-            System.out.println("|"+i);
-        }
-        System.out.println("----------------------------------------------------------");
-
     }
 }
